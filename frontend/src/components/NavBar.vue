c760495cae10b3708200ac989df7751b1d909cfd
Mark <googlcraftlol@gmail.com>
2022-01-11 05:15:53 +0300

    added mobile navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar">
+  <div id="nvb" class="navbar">
     <div class="navbar-elments">
       <img :src="logo" v-on:click="redirect">
       <div class="links">
@@ -32,7 +32,40 @@
     </div>
     <div class="navbar-mobile">
       <img :src="logo" class="navbar-image-mobile" v-on:click="redirect">
-      <h1>test</h1>
+      <div id="nvb-hamburger" class="navbar-hamburger" v-on:click="openMobileMenu()">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <div id="nvb-menu" class="navbar-mobile-menu">
+      <div class="links">
+        <ul>
+          
+          <li v-on:click="updateSelectedDivId('about');openMobileMenu();">
+            <router-link to="/">О нас</router-link>
+          </li>
+
+          <li v-on:click="updateSelectedDivId('services');openMobileMenu();">
+            <router-link to="/">Услуги</router-link>
+          </li>
+
+          <li v-on:click="updateSelectedDivId('news');openMobileMenu();">
+            <router-link to="/">Новости</router-link>
+          </li>
+          
+          <li v-on:click="updateSelectedDivId('audit');openMobileMenu();">
+            <router-link to="/">Аудиторские<br>Отчеты</router-link>
+          </li>
+          
+          <li v-on:click="updateSelectedDivId('contacts');openMobileMenu();">
+            <router-link to="/">Контакты</router-link>
+          </li>
+          
+          <li><SearchBox /></li>
+    
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -44,16 +77,31 @@
     name: "NavBar",
     components: {SearchBox},
     setup() {
+      let mobileMenuIsOpen = false
       return {
         logo,
+        mobileMenuIsOpen
       };
     },
     methods: {
-        updateSelectedDivId(id) {
-          this.$store.commit("currentDiv", id)  
+      updateSelectedDivId(id) {
+        this.$store.commit("currentDiv", id)  
       },
       redirect() {
         this.$router.push('/');
+      },
+      openMobileMenu() {
+        if (this.mobileMenuIsOpen) {
+          document.getElementById("nvb-hamburger").classList.remove("open")
+          document.getElementById("nvb").classList.remove("open")
+          document.getElementById("nvb-menu").classList.remove("open")
+          this.mobileMenuIsOpen = false
+        } else {
+          document.getElementById("nvb-hamburger").classList.add("open")
+          document.getElementById("nvb").classList.add("open")
+          document.getElementById("nvb-menu").classList.add("open")
+          this.mobileMenuIsOpen = true
+        }
       }
     }
   }
@@ -114,7 +162,7 @@
         color: black;
     }
 
-    .navbar-mobile {
+    .navbar-mobile, .navbar-mobile-menu {
       display: none;
     }
 
@@ -127,6 +175,18 @@
         display: block;
         width: 100%;
         height: 60px;
+        transition: height 400ms;
+      }
+
+      .navbar.open {
+        height: 510px;
+      }
+
+      .navbar-mobile-menu.open {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        justify-content: center;
       }
       
       .navbar-elments {
@@ -138,7 +198,7 @@
         justify-content: space-between;
         align-items: center;
         flex-direction: row;
-        height: 100%;
+        height: 60px;
         width: 90%;
         margin: auto;
       }
@@ -147,6 +207,72 @@
         width: 180px;
         height: 35px;
         cursor: pointer;
+      }
+
+      .navbar-hamburger {
+        width: 45px;
+        height: 45px;
+        -webkit-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+        -webkit-transition: .5s ease-in-out;
+        -moz-transition: .5s ease-in-out;
+        -o-transition: .5s ease-in-out;
+        transition: .5s ease-in-out;
+        cursor: pointer;
+      }
+
+      .navbar-hamburger span {
+        display: block;
+        position: absolute;
+        height: 9px;
+        width: 100%;
+        background: rgb(7, 191, 79);
+        border-radius: 9px;
+        opacity: 1;
+        left: 0;
+        -webkit-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+        -webkit-transition: .25s ease-in-out;
+        -moz-transition: .25s ease-in-out;
+        -o-transition: .25s ease-in-out;
+        transition: .25s ease-in-out;
+      }
+
+      .navbar-hamburger span:nth-child(1) {
+        top: 0px;
+      }
+
+      .navbar-hamburger span:nth-child(2) {
+        top: 18px;
+      }
+
+      .navbar-hamburger span:nth-child(3) {
+        top: 36px;
+      }
+
+      .navbar-hamburger.open span:nth-child(1) {
+        top: 18px;
+        -webkit-transform: rotate(135deg);
+        -moz-transform: rotate(135deg);
+        -o-transform: rotate(135deg);
+        transform: rotate(135deg);
+      }
+
+      .navbar-hamburger.open span:nth-child(2) {
+        opacity: 0;
+        left: -60px;
+      }
+
+      .navbar-hamburger.open span:nth-child(3) {
+        top: 18px;
+        -webkit-transform: rotate(-135deg);
+        -moz-transform: rotate(-135deg);
+        -o-transform: rotate(-135deg);
+        transform: rotate(-135deg);
       }
 
     }
